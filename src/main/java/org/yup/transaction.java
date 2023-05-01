c8857d4b814c5821d98d5ef09c8ba9096635fb7e
Jahi Lewis <jahifortislewis@gmail.com>
2023-05-01 11:23:13 -0400

    Creating necessary files for project

---
+++ src/main/java/org/yup/transaction.java
@@ -0,0 +1,58 @@
+package org.yup;
+
+public class transaction {
+
+private String date;
+private String time;
+private String description;
+private String vendor;
+private int amount;
+
+    public transaction(String date, String time, String description, String vendor, int amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
