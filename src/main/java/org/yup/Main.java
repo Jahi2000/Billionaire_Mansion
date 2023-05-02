0ee2f0603afd5b065bb40a0e1b28be27b06ff047
Jahi Lewis <jahifortislewis@gmail.com>
2023-05-02 10:03:18 -0400

    added the vendor search method

--- src/main/java/org/yup/Main.java
+++ src/main/java/org/yup/Main.java
@@ -3,14 +3,46 @@ package org.yup;
 import java.io.*;
 import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Scanner;
+import java.util.*;
 
 import static java.util.Comparator.comparing;
 
 public class Main {
+
+    public static void VenName() throws IOException {
+        Scanner scan = new Scanner (System.in);
+        FileReader theFileWeWantToWorkWith = new FileReader("transaction.csv");
+        BufferedReader theFileFromTheHDD = new BufferedReader(theFileWeWantToWorkWith);
+        String theLine;
+        ArrayList <transaction> list = new ArrayList<>();
+
+        // This loop reads the csv file by each line and stores it in an array
+        while ( ( theLine = theFileFromTheHDD.readLine() ) != null  ) {
+            String[] linePieces = theLine.split("\\|");
+            transaction T = new transaction(linePieces[0],linePieces[1],linePieces[2],linePieces[3],linePieces[4]);
+            list.add(T);
+
+        }
+
+          System.out.println("Please enter the name of the vendor");
+          String vendor = scan.nextLine();
+          //char v = vendor.charAt(0);
+          //char v1 = vendor.charAt(1);
+         //char v2 = vendor.charAt(2);
+          //char v3 = vendor.charAt(3);
+         // char v4 = vendor.charAt(4);
+
+        //this loop prints out the data in loop in a descending order by index numbers
+        for(int l = list.size() - 1; l < list.size() && l > 0; l--){
+            transaction P = list.get(l);
+            String ven = P.getVendor();
+            //System.out.println(vendor);
+            if(vendor.equalsIgnoreCase(P.getVendor().trim())) {
+               System.out.printf("%s | %s | %s | %s | %s |\n", P.getDate(), P.getTime(), P.getDescription(), P.getVendor(), P.getAmount());
+          }
+        }
+
+    }
 
     //This method only outputs the entries which were only deposits.
     public static void display_Deposits() throws IOException {
@@ -159,7 +191,7 @@ public class Main {
 
 
 
-     display_Deposits();
+     VenName();
 
 
 
