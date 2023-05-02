00f27088902d16517205ebca0f3b971a2247c27c
Jahi Lewis <jahifortislewis@gmail.com>
2023-05-01 23:27:40 -0400

    implemented the parts of the ledger method

--- src/main/java/org/yup/Main.java
+++ src/main/java/org/yup/Main.java
@@ -1,16 +1,99 @@
 package org.yup;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Scanner;
 
+import static java.util.Comparator.comparing;
+
 public class Main {
 
-    public static void Ledger(){
-        
+    //This method only outputs the entries which were only deposits.
+    public static void display_Deposits() throws IOException {
+        FileReader theFileWeWantToWorkWith = new FileReader("transaction.csv");
+        BufferedReader theFileFromTheHDD = new BufferedReader(theFileWeWantToWorkWith);
+        String theLine;
+        ArrayList <transaction> list = new ArrayList<>();
+
+        // This loop reads the csv file by each line and stores it in an array
+        while ( ( theLine = theFileFromTheHDD.readLine() ) != null  ) {
+            String[] linePieces = theLine.split("\\|");
+            transaction T = new transaction(linePieces[0],linePieces[1],linePieces[2],linePieces[3],linePieces[4]);
+            list.add(T);
+        }
+        //this loop prints out the data in loop in a descending order by index numbers
+        for(int l = list.size() - 1; l < list.size() && l > 0; l--){
+            transaction P = list.get(l);
+            char sym = P.getAmount().charAt(1);
+            if(!(sym == '-')) {
+                System.out.printf("%s | %s | %s | %s | %s |\n", P.getDate(), P.getTime(), P.getDescription(), P.getVendor(), P.getAmount());
+            }
+        }
+    }
+
+
+
+    //This method only outputs the entries which were debits
+    public static void display_Payments() throws IOException {
+        FileReader theFileWeWantToWorkWith = new FileReader("transaction.csv");
+        BufferedReader theFileFromTheHDD = new BufferedReader(theFileWeWantToWorkWith);
+        String theLine;
+        ArrayList <transaction> list = new ArrayList<>();
+
+        // This loop reads the csv file by each line and stores it in an array
+        while ( ( theLine = theFileFromTheHDD.readLine() ) != null  ) {
+            String[] linePieces = theLine.split("\\|");
+            transaction T = new transaction(linePieces[0],linePieces[1],linePieces[2],linePieces[3],linePieces[4]);
+            list.add(T);
+        }
+        //this loop prints out the data in loop in a descending order by index numbers
+        for(int l = list.size() - 1; l < list.size() && l > 0; l--){
+            transaction P = list.get(l);
+            char sym = P.getAmount().charAt(1);
+            if(sym == '-') {
+                System.out.printf("%s | %s | %s | %s | %s |\n", P.getDate(), P.getTime(), P.getDescription(), P.getVendor(), P.getAmount());
+            }
+            }
+    }
+
+
+
+    //this method display's all the ledger entries starting with the most recent.
+    public static void display_ALLLedger () throws IOException {
+        FileReader theFileWeWantToWorkWith = new FileReader("transaction.csv");
+        BufferedReader theFileFromTheHDD = new BufferedReader(theFileWeWantToWorkWith);
+        String theLine;
+        ArrayList <transaction> list = new ArrayList<>();
+
+        // This loop reads the csv file by each line and stores it in an array
+        while ( ( theLine = theFileFromTheHDD.readLine() ) != null  ) {
+            String[] linePieces = theLine.split("\\|");
+            transaction T = new transaction(linePieces[0],linePieces[1],linePieces[2],linePieces[3],linePieces[4]);
+            list.add(T);
+        }
+        //this loop prints out the data in loop in a descending order by index numbers
+        for(int l = list.size() - 1; l < list.size() && l > 0; l--){
+            transaction P = list.get(l);
+            System.out.printf("%s | %s | %s | %s | %s |\n", P.getDate(), P.getTime(), P.getDescription(), P.getVendor(), P.getAmount());
+        }
+    }
+    public static void Ledger() {
+
+        //Sys.out.print  //Would you like to display ledger entries ?
+
+        /* if yes {
+           call method display_ALLLedger
+    }
+*/
+
+
+
+
+
     }
 
     public static void MakePayment() throws IOException {
@@ -76,7 +159,7 @@ public class Main {
 
 
 
-
+     display_Deposits();
 
 
 
