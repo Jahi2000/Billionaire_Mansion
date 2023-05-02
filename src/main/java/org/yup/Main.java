313ced0498e678e4fb3bc256e3d3ba8fd20a6566
Jahi Lewis <jahifortislewis@gmail.com>
2023-05-02 15:54:31 -0400

    Created the MonthtoMethod and PreviousMonth method

--- src/main/java/org/yup/Main.java
+++ src/main/java/org/yup/Main.java
@@ -2,13 +2,87 @@ package org.yup;
 
 import java.io.*;
 import java.lang.reflect.Array;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 import static java.util.Comparator.comparing;
 
 public class Main {
+/*
+    // This method displays the previous month of the current month to the user.
+    public static void previousMonth() throws IOException {
+        Scanner scan = new Scanner(System.in);
+        FileReader theFileWeWantToWorkWith = new FileReader("transaction.csv");
+        BufferedReader theFileFromTheHDD = new BufferedReader(theFileWeWantToWorkWith);
+        String theLine;
+        ArrayList<transaction> list = new ArrayList<>();
 
+        // This loop reads the csv file by each line and stores it in an array
+        while ((theLine = theFileFromTheHDD.readLine()) != null) {
+            String[] linePieces = theLine.split("\\|");
+            transaction T = new transaction(linePieces[0], linePieces[1], linePieces[2], linePieces[3], linePieces[4]);
+            list.add(T);
+
+        }
+
+       /* Date today = new Date();
+        SimpleDateFormat domingo = new SimpleDateFormat("Y-MM");
+        String now = domingo.format(today);
+        int num = Integer.parseInt(now.substring(5,7));
+        num--;
+        String var = String.format("0%s",num);
+        SimpleDateFormat domingo1 = new SimpleDateFormat("Y");
+        String ahora = domingo1.format(today);
+        //System.out.print(ahora);
+
+
+        //this loop prints out the data in loop in a descending order by index numbers
+        for (int l = list.size() - 1; l < list.size() && l > 0; l--) {
+            transaction P = list.get(l);
+           // if (var.equals(P.getDate().substring(6,8).trim()) && ahora.equals(P.getDate().substring(0,5).trim())) {
+              System.out.printf("%s | %s | %s | %s | %s |\n", P.getDate(), P.getTime(), P.getDescription(), P.getVendor(), P.getAmount());
+            }
+        }
+
+*/
+
+
+    //This method allows the user's to get a report of the monthtodate transactions
+    public static void monthtodate() throws IOException {
+
+        Scanner scan = new Scanner(System.in);
+        FileReader theFileWeWantToWorkWith = new FileReader("transaction.csv");
+        BufferedReader theFileFromTheHDD = new BufferedReader(theFileWeWantToWorkWith);
+        String theLine;
+        ArrayList<transaction> list = new ArrayList<>();
+
+        // This loop reads the csv file by each line and stores it in an array
+        while ((theLine = theFileFromTheHDD.readLine()) != null) {
+            String [] linePieces;
+            linePieces = theLine.split("\\|");
+            System.out.println(linePieces[2]);
+            //transaction T = new transaction(linePieces[0], linePieces[1], linePieces[2], linePieces[3], linePieces[4]);
+           // list.add(T);
+
+        }
+
+        Date today = new Date();
+        SimpleDateFormat domingo = new SimpleDateFormat("Y-MM");
+        String now = domingo.format(today);
+
+        //this loop prints out the data in loop in a descending order by index numbers
+        for (int l = list.size() - 1; l < list.size() && l > 0; l--) {
+            transaction P = list.get(l);
+          if (now.equalsIgnoreCase(P.getDate().substring(0,8).trim())) {
+                System.out.printf("%s | %s | %s | %s | %s |\n", P.getDate(), P.getTime(), P.getDescription(), P.getVendor(), P.getAmount());
+            }
+
+        }
+
+    }
+
+    //  This method allows the user to search for a transaction by inserting the vendor's name
     public static void VenName() throws IOException {
         Scanner scan = new Scanner (System.in);
         FileReader theFileWeWantToWorkWith = new FileReader("transaction.csv");
@@ -26,17 +100,12 @@ public class Main {
 
           System.out.println("Please enter the name of the vendor");
           String vendor = scan.nextLine();
-          //char v = vendor.charAt(0);
-          //char v1 = vendor.charAt(1);
-         //char v2 = vendor.charAt(2);
-          //char v3 = vendor.charAt(3);
-         // char v4 = vendor.charAt(4);
+
 
         //this loop prints out the data in loop in a descending order by index numbers
         for(int l = list.size() - 1; l < list.size() && l > 0; l--){
             transaction P = list.get(l);
             String ven = P.getVendor();
-            //System.out.println(vendor);
             if(vendor.equalsIgnoreCase(P.getVendor().trim())) {
                System.out.printf("%s | %s | %s | %s | %s |\n", P.getDate(), P.getTime(), P.getDescription(), P.getVendor(), P.getAmount());
           }
@@ -191,7 +260,7 @@ public class Main {
 
 
 
-     VenName();
+     monthtodate();
 
 
 
